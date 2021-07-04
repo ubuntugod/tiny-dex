e7440de4e4de74a497ccec7df0ece1eb781ec451
Adarsh Ron <adarshron@gmail.com>
2021-07-04 08:56:53 +0530

    Upgraded the configs and contracts to solc0.8.6 and added totalSupply to the deployer

--- packages/blockchain/src/contracts/Token.sol
+++ packages/blockchain/src/contracts/Token.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0 <=0.8.6;
+pragma solidity ^0.8.6;
 
 contract Token {
     string public constant name = 'TinyDEX';
     string public constant symbol = 'TDEX';
     uint8 public constant decimals = 18;
     uint256 public constant totalSupply = 21_000_000e18;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
--- packages/blockchain/test/Token.test.js
+++ packages/blockchain/test/Token.test.js
@@ -3,7 +3,7 @@
 const Token = artifacts.require('Token');
 const expect = require('chai').use(require('chai-as-promised')).expect;
 
-contract('Token', () => {
+contract('Token', (accounts) => {
   let token;
   beforeEach(async () => {
     token = await Token.new();
@@ -23,16 +23,24 @@ contract('Token', () => {
       const result = await token.name();
       expect(result).to.equal(expectedResult.name);
     });
+
     it('Should have a valid symbol', async () => {
       const result = await token.symbol();
       expect(result).to.equal(expectedResult.symbol);
     });
+
     it('Should have valid decimals', async () => {
       const result = await token.decimals();
       expect(result.toNumber()).to.equal(expectedResult.decimals);
     });
+
     it('Should have a valid `totalSupply`', async () => {
       const result = await token.totalSupply();
+      expect(result.toString()).to.equal(expectedResult.totalSupply);
+    });
+
+    it('Should set the totalSupply to the account balance of the deployer', async () => {
+      const result = await token.balanceOf(accounts[0]);
       expect(result.toString()).to.equal(expectedResult.totalSupply);
     });
   });
